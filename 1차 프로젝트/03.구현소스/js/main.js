4a30033bd45e432c1a5e24f002a859e039f41403
Tom B. Parker <tombap8@hotmail.com>
2024-03-18 17:24:59 +0900

    ss

---
+++ 1차 프로젝트/03.구현소스/js/main.js
@@ -0,0 +1,10 @@
+// 메인 JS - main.js
+
+// 메인 동영상 파트 클릭시 이미지 보이기 숨기기
+
+const banArea = document.querySelector('#ban-area');
+const banImg = document.querySelector('.eve-ban');
+
+banArea.addEventListener('click', () => {
+  banImg.classList.toggle('on');
+});
