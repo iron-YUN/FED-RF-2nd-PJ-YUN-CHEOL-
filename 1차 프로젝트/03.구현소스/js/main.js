d8744be209eb3310c5687e38824d869f623ebcbc
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-03-21 18:20:04 +0900

    수정중

--- 1차 프로젝트/03.구현소스/js/main.js
+++ 1차 프로젝트/03.구현소스/js/main.js
@@ -3,8 +3,8 @@
 // 메인 동영상 파트 클릭시 이미지 보이기 숨기기
 
 const banArea = document.querySelector('#ban-area');
-const banImg = document.querySelector('.eve-ban');
-const banMv = document.querySelector('.eve-video');
+const banImg = document.querySelector('.baner');
+const banMv = document.querySelector('.ban-video');
 
 banArea.addEventListener('click', () => {
   banImg.classList.toggle('on');
