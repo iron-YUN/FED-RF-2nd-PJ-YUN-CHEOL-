5fd9b9911a3b45df26c1092ad99f862d64e5e6ba
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-04-12 15:33:11 +0900

    높이값 어떡할까나

---
+++ 1차 프로젝트/03.구현소스/js/footer_data.js
@@ -0,0 +1,34 @@
+const fgcon1 = {
+  "foot-menu1": [
+    "COLLECTIONS",
+    "CLASSIC RANGE",
+    "TRAVEL EXCLUSIVE",
+    "LIMITED RELEASES",
+    "LIFESTYLE & ACCESSORIES",
+    "ARCHIVE",
+  ],
+  "foot-menu2": [
+    "ABOUT",
+    "ABOUT THE MACALLAN",
+    "CAREERS",
+    "CONTACT US",
+    "NEWSROOM",
+    "SUSTAINABILITY",
+  ],
+  "foot-menu3": [
+    "SUPPORT",
+    "DELIVERY INFORMATION",
+    "FREQUENTLY ASKED QUESTIONS",
+    "MANAGE COOKIE PREFERENCES",
+    "MY ACCOUNT",
+    "RETURNS INFORMATION",
+  ],
+};
+const fgcon2 = {
+
+};
+const fgcon3 = {
+    
+};
+
+export {fgcon1,fgcon2,fgcon3};
