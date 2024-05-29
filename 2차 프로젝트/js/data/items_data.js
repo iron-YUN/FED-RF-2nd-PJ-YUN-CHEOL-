fec980268de02518e0a3d6fad97d8f1b2597cec6
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-05-29 17:37:47 +0900

    ㄹㅇㄹㅇㄹㅇㄹㅇ

--- 2차 프로젝트/js/data/items_data.js
+++ 2차 프로젝트/js/data/items_data.js
@@ -2,122 +2,65 @@
 // 메인분류 : 유형별
 // 서브분류 : 사용목적별
 // https://www.bose.com/c/headphones
-const products = {
-  Headphones: {
-    "Over-ear":[
-        {
-        // 서브1 그리드 이미지 내용
-        "product name":"Bose QuietComfort Ultra Headphones",
-        "price":"379",
-        "grid img":"",
-        "color":["Black","White Smoke","Sandstone"],
-        // 서브2 내용
-        "sub top img link":[],
-        "sub top video link":"",
-        "product info text":"World-class noise cancellation, quieter than ever before. Breakthrough spatialized audio for immersive listening, no matter the content or source. Elevated design and luxe materials for unrivaled comfort. It’s everything music makes you feel taken to new highs. Bose Immersive Audio pushes the boundary of what it means to listen by taking what you’re hearing out of your head and placing it in front of you. It sounds so real it’s almost like you could reach out and touch it.",
-        "What’s in the Box":[
-                                'Bose QuietComfort Ultra Headphones',
-                                'Carry case',
-                                '3.5 mm to 2.5 mm audio cable',
-                                'USB-C® (A to C) cable (20")',
-                                'Safety sheet',
-                            ],
-        "product info list":{
-                            "Headphone Fit":"	Around Ear Circumaural",
-                            "Headband":"	On Head Adjustable",
-                            "Cushions":"	Removable Cushion",
-                            "Microphones":"	Built-in Microphone",
-                            "Noise Cancelling":"	Yes",
-                            "Noise Control Type":"	Active Noise Cancelling, Echo Reduction, Adjustable Noise Cancelling",
-                            "Audio cable included":"	Yes",
-                            "Case":"	Carry",
-                            "Headphone":'	7.7" H x 5.5" W x 2.0" D (0.56 lb) ',
-                            "Product Material":"	Plastic, Aluminum, Leather (Protein)",
-                            "Product Case Material":"	Plastic (Hard)",
-                            "Ear Cushion Material":"	Protein Leather",
-                            "Rechargeable":"	Yes",
-                            "Battery Life":"	24 hours",
-                            "Battery Charge Time":"	3 hours",
-                            "Charging Accessory included":"	Yes",
-                            "Charging Interface(s)":"	USB C PORT",
-                            "Wireless Connectivity":"	Bluetooth, A2DP Bluetooth Audio Streaming, HFP Bluetooth, AVRCP Bluetooth, Bluetooth Low Energy",
-                            "Bluetooth Version":"	5.3",
-                            "Bluetooth Range":"	30'",
-                            "Bose App":"	Bose Music App",
-                             },
-        },
-        {
-        // 서브1 그리드 이미지 내용
-        "product name":"Bose Noise Cancelling Headphones 700",
-        "price":"299",
-        "grid img":"",
-        "color":["Black","Luxe Silver"],
-        // 서브2 내용
-        "sub top img link":[],
-        "sub top video link":"",
-        "product info text":"",
-        "What’s in the Box":[],
-        "product info list":[],
-        },
-        {
-        // 서브1 그리드 이미지 내용
-        "product name":"Bose QuietComfort Headphones",
-        "price":"249",
-        "grid img":"",
-        "color":["Black","White Smoke","Moonstone Blue","Cypress Green"],
-        // 서브2 내용
-        "sub top img link":[],
-        "top video link":"",
-        "product info text":"",
-        "What’s in the Box":[],
-        "product info list":[],
-        },
-    ],
-    "Aviation":[
-        {
-        // 서브1 그리드 이미지 내용
-        "product name":"Bose A30 Aviation Headset",
-        "price":"1299",
-        "grid img":"",
-        "color":["Black",],
-        // 서브2 내용
-        "sub top img link":[],
-        "top video link":"",
-        "product info text":"",
-        "What’s in the Box":[],
-        "product info list":[],
-         },
-        {
-        // 서브1 그리드 이미지 내용
-        "product name":"ProFlight Series 2 Aviation Headset",
-        "price":"1045",
-        "grid img":"",
-        "color":["Black",],
-        // 서브2 내용
-        "sub top img link":[],
-        "top video link":"",
-        "product info text":"",
-        "What’s in the Box":[],
-        "product info list":[],
-         },
-        {
-        // 서브1 그리드 이미지 내용
-        "product name":"A20 Aviation Headset with Bluetooth",
-        "price":"1095",
-        "grid img":"",
-        "color":["Black",],
-        // 서브2 내용
-        "sub top img link":[],
-        "top video link":"",
-        "product info text":"",
-        "What’s in the Box":[],
-        "product info list":[],
-         },
-    ],
+const productMainType = [
+  "Headphones",
+  "Earbuds",
+  "Speakers",
+  "Home Theater",
+  "PA Systems",
+];
+const productSubType = [
+"Over-ear",
+"Aviation",
+"In Ear",
+"Open Ear",
+"Portable",
+"Home Speakers",
+"Amplifiers",
+"Soundbars",
+"Bass modules",
+"Surround speakers",
+"Portable PA",
+"Array Systems",
+"Sub-woofers",
+"Mixers",
+"Sets",
+];
+const productList =[
+  {
+    dix:"",
+    name:"",
+    price:"",
+    MainType:"",
+    SubType:"",
+    color:[],
+    gridimg:"",
+    // 서브페이지에 들어갈 정보들
+    subPageImg:[],
+    infoText1:"",
+    infoIcon:{},
+    subTopVideo:"",
+    infoBox1:[],
+    infoBox2:{},
   },
-  Earbuds: {},
-  Speakers: {},
-  "Home Theater": {},
-  "PA Systems": {},
-  Sets: {},
+  ];
+
+const poductColor={
+"Black":"#000",
+"White Smoke":"#eee8e5",
+"Sandstone":"#928982",
+"Luxe Silver":"#adb0b1",
+"Cypress Green":"#537262",
+"Moonstone Blue":"#a2c2d9",
+"Chilled Lilac":"#c5c6e1",
+"Blue Dusk":"#486e8a",
+"Stone Blue":"#7f98a5",
+"Triple Black":"#283238",
+"Arctic White":"#f4f3ef",
+"Bose Black":"#202020",
+"bgc1":"#181818",
+"bgc2":"#191919",
+"bgc3":"#0d0d0d",
+"bgc4":"#282828",
 };
+
