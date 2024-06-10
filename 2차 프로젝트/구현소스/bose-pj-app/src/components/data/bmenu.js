5dcedf0cc87b03c018a63c30bf3475eb2f2a71b7
iron-YUN <159221265+iron-YUN@users.noreply.github.com>
2024-06-10 10:14:54 +0900

    몰랑

--- 2차 프로젝트/구현소스/bose-pj-app/src/components/data/bmenu.js
+++ 2차 프로젝트/구현소스/bose-pj-app/src/components/data/bmenu.js
@@ -2,127 +2,139 @@
 // https://www.bose.com/c/headphones
 const bmData = {
   "Customer Care": [
-    { text: "Troubleshooting", link: "https://support.bose.com/s/bose/product-support-home?language=en_US" },
-    { text: "Repair & Replacement", link: "https://support.bose.com/s/guided-trouble-shooter-splash-page?language=en_US" },
+    {
+      text: "Troubleshooting",
+      link: "https://support.bose.com/s/bose/product-support-home?language=en_US",
+    },
+    {
+      text: "Repair & Replacement",
+      link: "https://support.bose.com/s/guided-trouble-shooter-splash-page?language=en_US",
+    },
     { text: "Order Tracking", link: "https://www.bose.com/trackorder" },
-    { text: "My Bose Sign In / Register", link: "https://www.bose.com/my-account" },
-    { text: "Register Your Product", link: "https://www.bose.com/product-registration" },
-    { text: "Contact Us", link: "https://support.bose.com/s/get-help-landing-page?language=en_US" },
+    {
+      text: "My Bose Sign In / Register",
+      link: "https://www.bose.com/my-account",
+    },
+    {
+      text: "Register Your Product",
+      link: "https://www.bose.com/product-registration",
+    },
+    {
+      text: "Contact Us",
+      link: "https://support.bose.com/s/get-help-landing-page?language=en_US",
+    },
   ],
   "Our company": [
     {
-        text:"About Us",
-        link:"https://www.bose.com/about",
+      text: "About Us",
+      link: "https://www.bose.com/about",
     },
     {
-        text:"Find a Store",
-        link:"https://www.bose.com/stores?showMap=true",
+      text: "Find a Store",
+      link: "https://www.bose.com/stores?showMap=true",
     },
     {
-        text:"ESG",
-        link:"https://www.bose.com/esg",
+      text: "ESG",
+      link: "https://www.bose.com/esg",
     },
     {
-        text:"Careers",
-        link:"https://www.bose.com/about/careers",
+      text: "Careers",
+      link: "https://www.bose.com/about/careers",
     },
     {
-        text:"Press Room",
-        link:"https://www.bose.com/pressroom",
+      text: "Press Room",
+      link: "https://www.bose.com/pressroom",
     },
     {
-        text:"Stories",
-        link:"https://www.bose.com/stories",
+      text: "Stories",
+      link: "https://www.bose.com/stories",
     },
     {
-        text:"Partnerships & Licensing",
-        link:"https://www.bose.com/partnerships-and-licensing",
+      text: "Partnerships & Licensing",
+      link: "https://www.bose.com/partnerships-and-licensing",
     },
   ],
   Offers: [
     {
-        text:"Gift Cards",
-        link:"https://www.bose.com/gift-cards",
+      text: "Gift Cards",
+      link: "https://www.bose.com/gift-cards",
     },
     {
-        text:"ID.me Group Program",
-        link:"https://www.bose.com/idme-group-discounts-program",
+      text: "ID.me Group Program",
+      link: "https://www.bose.com/idme-group-discounts-program",
     },
     {
-        text:"Bulk Order Program",
-        link:"https://www.bose.com/bulk-order-discount-program",
+      text: "Bulk Order Program",
+      link: "https://www.bose.com/bulk-order-discount-program",
     },
     {
-        text:"Partner & Employee Program",
-        link:"https://www.bose.com/partner-employee-discount-program",
+      text: "Partner & Employee Program",
+      link: "https://www.bose.com/partner-employee-discount-program",
     },
     {
-        text:"Certified Refurbished",
-        link:"https://www.bose.com/c/refurbished",
+      text: "Certified Refurbished",
+      link: "https://www.bose.com/c/refurbished",
     },
     {
-        text:"Trade Up",
-        link:"https://www.bose.com/trade-up",
+      text: "Trade Up",
+      link: "https://www.bose.com/trade-up",
     },
   ],
   "Additional Links": [
     {
-        text:"Automotive",
-        link:"https://automotive.bose.com/?utm_source=bose.com&utm_medium=referral&utm_content=footer-link",
+      text: "Automotive",
+      link: "https://automotive.bose.com/?utm_source=bose.com&utm_medium=referral&utm_content=footer-link",
     },
     {
-        text:"Reseller Portal",
-        link:"https://reseller.bose.com/en_us/landingpage.html",
+      text: "Reseller Portal",
+      link: "https://reseller.bose.com/en_us/landingpage.html",
     },
   ],
 };
 
-const bmData2 =[
-    {
-        text:"Bose app",
-        link:"https://www.bose.com/apps/bose-app",
-    },
-    {
-        text:"Bose Connect App",
-        link:"https://www.bose.com/apps/bose-connect",
-    },
-]
-const bmData3 =[
-    {
-        text:"Sitemap",
-        link:"https://www.bose.com/sitemap",
-    },
-    {
-        text:"Legal",
-        link:"https://www.bose.com/legal",
-    },
-    {
-        text:"Privacy Policy",
-        link:"https://www.bose.com/legal/privacy-policy",
-    },
-    {
-        text:"CA Notice of Collection",
-        link:"https://assets.bosecreative.com/m/14802bcc3901876e/original/California_Privacy_Notice_of_Collection_Dec_2022_v2.pdf",
-    },
-    {
-        text:"Your privacy choices",
-        link:"#",
-    },
-    {
-        text:"Cookies Notice",
-        link:"https://www.bose.com/legal/cookies-notice",
-    },
-    {
-        text:"Terms of Sale",
-        link:"https://www.bose.com/legal/terms-of-sale",
-    },
-    {
-        text:"Terms of Use",
-        link:"https://www.bose.com/legal/terms-of-use",
-    },
-]
+const bmData2 = [
+  {
+    text: "Bose app",
+    link: "https://www.bose.com/apps/bose-app",
+  },
+  {
+    text: "Bose Connect App",
+    link: "https://www.bose.com/apps/bose-connect",
+  },
+];
+const bmData3 = [
+  {
+    text: "Sitemap",
+    link: "https://www.bose.com/sitemap",
+  },
+  {
+    text: "Legal",
+    link: "https://www.bose.com/legal",
+  },
+  {
+    text: "Privacy Policy",
+    link: "https://www.bose.com/legal/privacy-policy",
+  },
+  {
+    text: "CA Notice of Collection",
+    link: "https://assets.bosecreative.com/m/14802bcc3901876e/original/California_Privacy_Notice_of_Collection_Dec_2022_v2.pdf",
+  },
+  {
+    text: "Your privacy choices",
+    link: "#",
+  },
+  {
+    text: "Cookies Notice",
+    link: "https://www.bose.com/legal/cookies-notice",
+  },
+  {
+    text: "Terms of Sale",
+    link: "https://www.bose.com/legal/terms-of-sale",
+  },
+  {
+    text: "Terms of Use",
+    link: "https://www.bose.com/legal/terms-of-use",
+  },
+];
 
-
-
-
-export {bmData,bmData2,bmData3}
+export { bmData, bmData2, bmData3 };
